08cbf1508335946bd2d658521dd387be752618f1
lubumbax <murojc@gmail.com>
2019-11-25 19:05:12 +0100

    Initial commit

---
+++ be/flights-service/src/main/java/com/lubumbax/linkitair/flights/FlightsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.lubumbax.linkitair.flights;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlightsServiceApplication.class, args);
+    }
+
+}
---
+++ be/flights-service/src/test/java/com/lubumbax/linkitair/flights/FlightsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lubumbax.linkitair.flights;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
