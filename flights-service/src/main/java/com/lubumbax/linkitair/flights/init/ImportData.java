205e40a455ea42ee2ab1383122c6e701c2492be4
lubumbax <murojc@gmail.com>
2022-11-28 17:25:36 +0100

    Remove unneeded import

--- flights-service/src/main/java/com/lubumbax/linkitair/flights/init/ImportData.java
+++ flights-service/src/main/java/com/lubumbax/linkitair/flights/init/ImportData.java
@@ -11,7 +11,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
-import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
